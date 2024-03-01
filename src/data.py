d61bcd2511dcd938cb7b29cda6e014a76c646f01
Chloe <chloe.lere@epitech.eu>
2024-03-01 12:01:02 +0100

    make one table for every file about the parameter 10 : add every suntime by day then make the average for each sation

--- src/data.py
+++ src/data.py
@@ -37,13 +37,36 @@ def weather_average_by_day(folder_path, column_name_average, column_to_rm = ["Kv
     merged_data = merged_data[(merged_data['Datum'] >= start_date) & (merged_data['Datum'] <= end_date)]
 
     columns_to_average = merged_data.select_dtypes(include=['int', 'float']).columns
-
     merged_data[column_name_average] = merged_data[columns_to_average].mean(axis=1)
-
     data = merged_data[['Datum', column_name_average]]
     return (data)
+
+def weather_parameter_10(folder_path):
+    files = os.listdir(folder_path)
+    first_round = True
+
+    for filename in files:
+        if first_round:
+            merged_data = pd.read_csv(folder_path + filename).drop(columns=["Kvalitet"])
+            first_round = False
+            merged_data['Datum'] = pd.to_datetime(merged_data['Datum'])
+            merged_data = merged_data.groupby(merged_data['Datum'].dt.date)['Solskenstid'].sum().reset_index()
+            print(merged_data.head())
+            continue
+        current = pd.read_csv(folder_path + filename).drop(columns=["Kvalitet"])
+        current['Datum'] = pd.to_datetime(current['Datum'])
+        current = current.groupby(current['Datum'].dt.date)['Solskenstid'].sum().reset_index()
+        station_number = re.findall(r'\d+', filename)[0]
+        merged_data = pd.merge(merged_data, current, on='Datum', how='outer', suffixes=('', f'_{station_number}'))
+     
+    columns_to_average = merged_data.select_dtypes(include=['int', 'float']).columns
+    merged_data["AverageSolskenstid"] = merged_data[columns_to_average].mean(axis=1)
+    data = merged_data[['Datum', "AverageSolskenstid"]]
+    return(data)
+    
 
 def get_table():
     weather_average_by_day("../data/wettersol/wettersol/smhi_data_2022-today/parameter_2/", "AverageLufttemperatur")
     weather_average_by_day("../data/wettersol/wettersol/smhi_data_2022-today/parameter_5/", "AverageNederbördsmängd")
     weather_average_by_day("../data/wettersol/wettersol/smhi_data_2022-today/parameter_8/", "AverageSnödjup", ["Kvalitet", "Tid (UTC)"])
+    weather_parameter_10("../data/wettersol/wettersol/smhi_data_2022-today/parameter_10/")
