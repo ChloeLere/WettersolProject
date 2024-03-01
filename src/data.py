49fb4005396f5b67851164db9ae412217e0f4dfd
Chloe <chloe.lere@epitech.eu>
2024-03-01 10:55:25 +0100

    compute average for all station for parameter 2, 5, and 8 of the weather data

--- src/data.py
+++ src/data.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
+import re
+pd.set_option('display.max_rows', None) 
+pd.set_option('display.max_columns', None) 
 
 def get_data(filename):
     file_result = pd.read_csv(filename)
@@ -12,3 +16,34 @@ def plot_data(data, xlabel, ylabel, graph_name):
     plt.ylabel(ylabel)
     plt.title(graph_name)
     plt.show()
+
+
+def weather_average_by_day(folder_path, column_name_average, column_to_rm = ["Kvalitet"]):
+    files = os.listdir(folder_path)
+    first_round = True
+    start_date = pd.Timestamp("2022-01-01")
+    end_date = pd.Timestamp('2024-01-01')
+
+    for filename in files:
+        if first_round:
+            merged_data = pd.read_csv(folder_path + filename).drop(columns=column_to_rm)
+            first_round = False
+            continue
+        current = pd.read_csv(folder_path + filename).drop(columns=column_to_rm)
+        station_number = re.findall(r'\d+', filename)[0]
+        merged_data = pd.merge(merged_data, current, on='Datum', how='outer', suffixes=('', f'_{station_number}'))
+
+    merged_data['Datum'] = pd.to_datetime(merged_data['Datum'])
+    merged_data = merged_data[(merged_data['Datum'] >= start_date) & (merged_data['Datum'] <= end_date)]
+
+    columns_to_average = merged_data.select_dtypes(include=['int', 'float']).columns
+
+    merged_data[column_name_average] = merged_data[columns_to_average].mean(axis=1)
+
+    data = merged_data[['Datum', column_name_average]]
+    return (data)
+
+def get_table():
+    weather_average_by_day("../data/wettersol/wettersol/smhi_data_2022-today/parameter_2/", "AverageLufttemperatur")
+    weather_average_by_day("../data/wettersol/wettersol/smhi_data_2022-today/parameter_5/", "AverageNederbördsmängd")
+    weather_average_by_day("../data/wettersol/wettersol/smhi_data_2022-today/parameter_8/", "AverageSnödjup", ["Kvalitet", "Tid (UTC)"])
