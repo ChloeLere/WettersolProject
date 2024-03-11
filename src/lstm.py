096183a7031d6925304b9a19882ca28c3efdc2d2
Chloe <chloe.lere@epitech.eu>
2024-03-11 14:23:07 +0100

    edit lstm algo

--- src/lstm.py
+++ src/lstm.py
@@ -65,56 +65,12 @@ class MyLSTM:
     def train_model(self):
         x_train, y_train, x_val, y_val, x_test, y_test = self.split_data()
 
-        callbacks = [keras.callbacks.EarlyStopping(monitor='val_loss', patience=2,verbose=0)]
+        #callbacks = [keras.callbacks.EarlyStopping(monitor='val_loss', patience=2,verbose=0)]
+        #self.model.fit(x_train, y_train, epochs=100, batch_size=32, verbose=1,validation_split=0.05, callbacks=callbacks)
         
-        self.model.fit(x_train, y_train, epochs=20, batch_size=16, verbose=1,validation_split=0.05, callbacks=callbacks)
+        self.model.fit(x_train, y_train, epochs=50, batch_size=32, validation_data=(x_val, y_val), verbose=1)
 
-        #history = model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_val, y_val), verbose=1)
+        loss = self.model.evaluate(x_test, y_test)
 
-        #loss = model.evaluate(X_test, y_test)
-
-        #predictions = model.predict(X_test)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        #scaler = MinMaxScaler()
-        #self.data = pd.DataFrame(scaler.fit_transform(data), columns=data.columns, index=data.index)
-        #print(target.head())
-        #print(self.data.head())
-        #self.target = pd.DataFrame(scaler.fit_transform(target), columns=target.columns, index=target.index)
-        #num_time_steps = self.data.shape[0]
-        #num_train, num_val = (
-        #    int(num_time_steps * train_size),
-        #    int(num_time_steps * val_size),
-        #)
-        #train_array = self.data[:num_train]
-        #mean, std = train_array.mean(axis=0), train_array.std(axis=0)
-        #self.train_data = (train_array - mean) / std
-        #self.valid_data = (self.data[num_train : (num_train + num_val)] - mean) / std
-        #self.test_data = (self.data[(num_train + num_val) :] - mean) / std
-        #self.train_target = (train_array - mean) / std
-        #self.valid_target = (self.target[num_train : (num_train + num_val)] - mean) / std
-        #self.test_target = (self.target[(num_train + num_val) :] - mean) / std
-        #print(f"train set size: {self.train_data.shape}")
-        #print(f"validation set size: {self.valid_data.shape}")
-        #print(f"test set size: {self.test_data.shape}")
-
-
+        predictions = self.model.predict(x_test)
+ 
