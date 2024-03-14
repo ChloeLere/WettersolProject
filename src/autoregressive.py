3598c60250a134304dde5ed3574936e310f6f80d
schneider <Yobobis@gmail.com>
2024-03-14 10:52:51 +0100

    autoregressive chopped train -> train & predict

--- src/autoregressive.py
+++ src/autoregressive.py
@@ -25,21 +25,34 @@ class MyAutoregressive:
         return predictions
 
     def train(self, offset=0, training_size=320, testing_size=5):
-        x_train, y_train, x_test, y_test = time_split(self.variables, self.target, offset=offset, training_size=training_size, testing_size=testing_size)
+        self.XYs = time_split(self.variables, self.target, offset, training_size, testing_size)
+        x_train, y_train, _, _ = self.XYs
         
-        model = AutoReg(y_train, lags=self.lags, exog=x_train)
-        model_fit = model.fit()
-        print('Coefficients: %s' % model_fit.params)
-        predictions = model_fit.predict(start=len(x_train), end=len(x_train) + len(x_test) - 1, exog_oos=x_test)
+        self.model = AutoReg(y_train, lags=self.lags, exog=x_train)
+        self.model_fit = self.model.fit()
 
+    def predict(self, display=False):
+        x_train, _, x_test, y_test = self.XYs
+        predictions = self.model_fit.predict(start=len(x_train), end=len(x_train) + len(x_test) - 1, exog_oos=x_test)
         predictions = self.clamp_negative(predictions)
-        for i in range(len(predictions)):
-            print('predicted=%f, expected=%f' % (predictions[i], y_test[i]))
+
+        if display:
+            for i in range(len(predictions)):
+                print('predicted=%f, expected=%f' % (predictions[i], y_test[i]))
         
         self.mse = mean_squared_error(y_test, predictions)
         self.rmse = self.mse ** 0.5
         self.r_squared = r2_score(y_test, predictions)
         self.mae = mean_absolute_error(y_test, predictions)
+
+    # get the metrics of the last train run
+    def metrics(self):
+        print("=================================AutoRegressive=================================")
+        print("Mean Squared Error:", self.mse)
+        print("Mean Absolute Error:", self.mae)
+        print("Root Mean Squared Error:", self.rmse)
+        print("R Squared score:", self.r_squared)
+        return self.mse, self.mae, self.rmse, self.r_squared
 
     # don't use
     def train_2_unused(self):
@@ -64,12 +77,3 @@ class MyAutoregressive:
         #predictions = model_fit.predict(start=len(x_train), end=len(x_train) + len(x_test) - 1, exog_oos=x_test)    
         for i in range(len(y_pred)):
             print('predicted=%f, expected=%f' % (y_pred[i], y_test[i]))
-
-    # get the metrics of the last train run
-    def metrics(self):
-        print("=================================AutoRegressive=================================")
-        print("Mean Squared Error:", self.mse)
-        print("Mean Absolute Error:", self.mae)
-        print("Root Mean Squared Error:", self.rmse)
-        print("R Squared score:", self.r_squared)
-        return self.mse, self.mae, self.rmse, self.r_squared
